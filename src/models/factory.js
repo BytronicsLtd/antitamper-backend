16f34bab30ddb7feac18aca49ef833121e061ed3
unknown <note5mn@gmail.com>
2025-02-09 00:08:57 +0300

    eased auth on  get data

--- src/models/factory.js
+++ src/models/factory.js
@@ -3,32 +3,33 @@ const Schema = mongoose.Schema;
 const mongoosePaginate = require('mongoose-paginate-v2');
 
 const schema = new Schema({
-  name: { 
-    type: String, 
-    required: true 
+  name: {
+    type: String,
+    required: true,
+    unique: false,
   },
-  location: { 
-    type: String, 
-    required: true 
+  location: {
+    type: String,
+    required: true
   },
-  employees: [{ 
-    type: Schema.Types.ObjectId, 
-    ref: 'User' 
+  employees: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
   }],
-  status: { 
-    type: String, 
+  status: {
+    type: String,
     enum: ['active', 'inactive'],
-    default: 'active' 
+    default: 'active'
   }
-}, { 
-  timestamps: true 
+}, {
+  timestamps: true
 });
 
 // Add index for better query performance
 schema.index({ name: 1, location: 1 }, { unique: true });
 
 // Add any methods you might need
-schema.methods.getActiveEmployees = function() {
+schema.methods.getActiveEmployees = function () {
   return this.model('User').find({
     _id: { $in: this.employees },
     status: 'active'
@@ -38,8 +39,8 @@ schema.methods.getActiveEmployees = function() {
 schema.plugin(mongoosePaginate);
 
 schema.method('toJSON', function () {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 module.exports = mongoose.model('Factory', schema, "factories");
