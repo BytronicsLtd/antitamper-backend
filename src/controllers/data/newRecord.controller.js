ccbd6c7e05bea5979b0c65f8d20c00ca195f51d8
unknown <note5mn@gmail.com>
2025-06-23 20:35:37 +0300

    uncommented mqtt

--- src/controllers/data/newRecord.controller.js
+++ src/controllers/data/newRecord.controller.js
@@ -191,7 +191,6 @@ function validateInterrupts({ data, last_entry }) {
                 // Calibration switch check (highest priority)
                 if (priority_type === "calibration switch") {
                     let calib_sw_interrupt_events = data.calib_sw_interrupt_events.split(" ").map(event => event.trim());
-                    console.log("calibration switch events array ", calib_sw_interrupt_events);
                     if (calib_sw_interrupt_events.includes("1")) {
                         new_data.alert_types.push("calibration-switch");
                     }
@@ -199,14 +198,13 @@ function validateInterrupts({ data, last_entry }) {
                 // Enclosure check (second priority) 
                 if (priority_type === "enclosure") {
                     let enclosure_interrupt_events = data.enclosure_interrupt_events.split(" ").map(event => event.trim());
-                    console.log("enclosure events array ", enclosure_interrupt_events);
                     if (enclosure_interrupt_events.includes("1")) {
                         new_data.alert_types.push("enclosure");
                     }
                 }
             }
             // Enclosure check (second priority) 
-            if (data.battery_voltage < 3.4 && !battery_alert_added) {
+            if (data.battery_voltage < 3.5 && !battery_alert_added) {
                 new_data.alert_types.push("battery-voltage");
                 battery_alert_added = true;
             }
