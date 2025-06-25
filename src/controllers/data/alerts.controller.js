d7d91216e50a32a834e20d1af3edcbd4c9e67891
unknown <note5mn@gmail.com>
2025-06-25 10:48:23 +0300

    updated how we check for alert types array

--- src/controllers/data/alerts.controller.js
+++ src/controllers/data/alerts.controller.js
@@ -62,6 +62,9 @@ const controller = {
       }
       if (interrupt_types) {
         query.interrupt_types = interrupt_types;
+        if (interrupt_types.battery_voltage) {
+          query.alert_types = { $elemMatch: { $eq: "battery-voltage" } }
+        }
       }
       if (enclosure) {
         query.enclosure = enclosure;
